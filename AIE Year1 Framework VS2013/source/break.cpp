5a7b96e49fcc5618e24f70abfa7888252a0118dd
JHL1138 <jhlambe@gmail.com>
2014-09-15 13:17:41 -0700

    ZIP FILES UPDATE

---
+++ AIE Year1 Framework VS2013/source/break.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void main()
+for (int i = 0; i < 10; i++)
+{
+	if (i >4)
+	{
+		break;
+	}
+
+	std::cout i << std::endl;
+
+	system("pause");
+}
---
+++ AIE Year1 Framework VS2013/source/loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void main()
+
+{
+	{
+		bool playDead = false;
+		while (!playerDead)
+
+		{
+			cout "player is not Dead";
+		}
+	}
+	system("pause"):
+}
